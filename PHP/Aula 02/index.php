df7e865f4262bb1b44a5c8d57800739ec32836d6
luisgbcorona-create <luis.gbcorona@aluno.edu.es.gov.br>
2026-06-24 10:54:46 -0300

    Create index.php

---
+++ PHP/Aula 02/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title> Aula 2 -  </title>
+    </head>
+    <body>
+        <?php
+            $nome = "Luís";
+            $idade = "16";
+            echo "Meu nome é " .$nome. " e tenho " .$idade. " anos.";
+        ?>
+    </body>
+</html>
